f53dcb37c738041e1283d51ee11097b4438b63ea
rsamek <rafal.samek@gmail.com>
2024-04-14 10:45:05 +0200

    Repository

---
+++ src/main/java/pl/edu/wszib/springjpa/service/TodoService.java
@@ -0,0 +1,42 @@
+package pl.edu.wszib.springjpa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.edu.wszib.springjpa.model.ToDo;
+import pl.edu.wszib.springjpa.model.ToDoStatus;
+import pl.edu.wszib.springjpa.repository.TodoRepository;
+
+import java.util.List;
+
+@Service
+public class TodoService implements CrudService<ToDo, Integer> {
+    @Autowired
+    private TodoRepository repository;
+
+    @Override
+    public List<ToDo> list() {
+        return repository.findAll();
+    }
+
+    @Override
+    public ToDo get(Integer id) {
+        return repository.getReferenceById(id);
+    }
+
+    @Override
+    public ToDo create(ToDo toDo) {
+        toDo.setId(null);
+        toDo.setStatus(ToDoStatus.NEW);
+        return repository.save(toDo);
+    }
+
+    @Override
+    public ToDo update(ToDo toDo) {
+        return repository.save(toDo);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+}
